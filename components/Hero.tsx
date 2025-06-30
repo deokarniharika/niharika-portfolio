0a00a3a728006074d26ee25eb44c8e74b49033d1
deokarniharika <1032200921@mitwpu.ed.uin>
2025-06-30 04:18:04 +0100

    Update: added new info and changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section id="hero" className="text-center">
       <h1 className="text-4xl md:text-6xl font-bold">Hi, I'm Niharika Deokar</h1>
-      <p className="mt-4 text-lg">I build delightful things on the web 💫</p>
+      <p className="mt-4 text-lg">Building ML Models that Matter | Data Science intern @Fidelis AI | Python • SQL • AWS • Tableau | Outstanding PLUS Awardee 💫</p>
     </section>
   )
 }
